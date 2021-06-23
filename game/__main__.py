a2f7fe86f2f7ac7965d5418ad2a8323ba1049189
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-06-23 11:18:10 -0600

    Adding physics skeleton.

--- game/__main__.py
+++ game/__main__.py
@@ -1,64 +1,72 @@
 import random
+import arcade
+
 from game import constants
+
 from game.director import Director
-from game.actor import Actor
+from game.entity import Entity
 from game.point import Point
-from game.control_actors_action import ControlActorsAction
+from game.controls import controls
 from game.draw_actors_action import DrawActorsAction
-from game.handle_collisions_action import HandleCollisionsAction
-from game.move_actors_action import MoveActorsAction
+#from game.handle_collisions_action import HandleCollisionsAction
+#from game.move_actors_action import MoveActorsAction
+from game.physics import Physics
 from game.input_service import InputService
 from game.output_service import OutputService
-from asciimatics.screen import Screen 
 
 def main(screen):
 
-    # create the cast {key: tag, value: list}
-    cast = {}
+    # create the entities {key: tag, value: list}
+    entities = {}
 
     x = int(constants.MAX_X / 2)
     y = int(constants.MAX_Y - 1)
     position = Point(x, y)
-    paddle = Actor()
+    paddle = Entity()
     paddle.set_text("===========")
     paddle.set_position(position)
-    cast["paddle"] = [paddle]
+    entities["paddle"] = [paddle]
 
-    cast["brick"] = []
+    entities["brick"] = []
     for x in range(5, 75):
         for y in range(2, 6):
             position = Point(x, y)
-            brick = Actor()
+            brick = Entity()
             brick.set_text("*")
             brick.set_position(position)
-            cast["brick"].append(brick)
+            entities["brick"].append(brick)
 
     x = int(constants.MAX_X / 2)
     y = int(constants.MAX_Y / 2)
     position = Point(x, y)
     velocity = Point(1, -1)
-    ball = Actor()
+    ball = Entity()
     ball.set_text("@")
     ball.set_position(position)
     ball.set_velocity(velocity)
-    cast["ball"] = [ball]
+    entities["ball"] = [ball]
     
     # create the script {key: tag, value: list}
     script = {}
 
-    input_service = InputService(screen)
-    output_service = OutputService(screen)
-    control_actors_action = ControlActorsAction(input_service)
-    move_actors_action = MoveActorsAction()
-    handle_collisions_acition = HandleCollisionsAction()
+    physics = Physics
+
+    input_service = ArcadeInputService()
+    output_service = ArcadeOutputService()
+    
+    playerControls = controls(input_service)
+    moveTick = physics.moveTick()
+    physicsTick = physics.physicsTick()
     draw_actors_action = DrawActorsAction(output_service)
     
-    script["input"] = [control_actors_action]
-    script["update"] = [move_actors_action, handle_collisions_acition]
+    script["input"] = [playerControls]
+    script["update"] = [moveTick, physicsTick]
     script["output"] = [draw_actors_action]
 
     # start the game
-    director = Director(cast, script)
-    director.start_game()
+    director = Director(entities, script)
+    director.setup()
+    arcade.run()
 
-Screen.wrapper(main)
+if __name__ == "__main__":
+    main()
---
+++ game/game/physics.py
@@ -0,0 +1,10 @@
+class Physics():
+    
+    def __init__(self):
+        pass
+
+    def moveTick(self, entity):
+        pass
+
+    def physicsTick(self, entity):
+        pass
