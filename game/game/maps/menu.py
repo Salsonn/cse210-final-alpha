63ecf692ec21b690b2905a86a81d650188ac7c35
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-06-30 18:52:42 -0600

    Manually merging maps into main

--- game/game/maps/menu.py
+++ game/game/maps/menu.py
@@ -6,10 +6,10 @@ class MainMenu():
 
     def __init__(self):
         self._BACKGROUND_COLOR = arcade.color.BLACK
-        self._FLOOR = './game/images/floor_tile_sprite.png'
+        self._FLOOR = './images/floor_tile_sprite.png'
         self._FLOOR_W = 32
         self._FLOOR_H = 32
-        self._WALL = './game/images/wall_tile_sprite.png'
+        self._WALL = './images/wall_tile_sprite.png'
         self._WALL_W = 32
         self._WALL_H = 32
         self._FONT_COLOR = arcade.color.PALE_BLUE
@@ -30,10 +30,47 @@ class MainMenu():
     def drawMap(self):
         
         # Draw the Walls
-        arcade.draw_rectangle_filled(self._LEFT_WALL_X, self._LEFT_WALL_Y, 20, constants.windowY, self._WALL_COLOR_2)
-        arcade.draw_rectangle_filled(constants.windowX, self._LEFT_WALL_Y, 20, constants.windowY, self._WALL_COLOR_2)
-        arcade.draw_rectangle_filled(constants.windowX / 2, constants.windowY, constants.windowX, 20, self._WALL_COLOR_2)
-        arcade.draw_rectangle_filled(constants.windowX / 2, 0, constants.windowX, 20, self._WALL_COLOR_2)
+        arcade.draw_rectangle_filled(self._LEFT_WALL_X, self._LEFT_WALL_Y, 20, constants.windowY, self._WALL_COLOR)
+        arcade.draw_rectangle_filled(constants.windowX, self._LEFT_WALL_Y, 20, constants.windowY, self._WALL_COLOR)
+        arcade.draw_rectangle_filled(constants.windowX / 2, constants.windowY, constants.windowX, 20, self._WALL_COLOR)
+        arcade.draw_rectangle_filled(constants.windowX / 2, 0, constants.windowX, 20, self._WALL_COLOR)
 
         # Welcome Message
         arcade.draw_text(self._INFO, constants.windowX / len(self._TITLE) + 100, constants.windowY - 40, self._FONT_COLOR, 25, 340, 'center', 'calibri', True)
+
+        # Draw Walls to choose what to do next
+        self.wall_list_h = arcade.SpriteList()
+
+        for i in range(constants.windowY // self._WALL_H - 8):
+
+            # Create the floor instance
+            wall = arcade.Sprite(self._WALL, 1)
+
+            # Position the floor sprites
+            wall.center_x = i * self._COLUMN_SPACING + self._LEFT_MARGIN
+            wall.center_y = self._ROW_SPACING + self._BOTTOM_MARGIN + 400
+
+            # Add the floor to the lists
+            self.wall_list_h.append(wall)
+            self.wall_list_h.draw()
+
+        arcade.draw_text('Level 1', wall.center_x, constants.windowY - 150, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
+
+             # Draw Walls to choose what to do next
+        self.wall_list_v = arcade.SpriteList()
+
+        for j in range(constants.windowY // self._WALL_H - 8):
+
+            # Create the floor instance
+            wall = arcade.Sprite(self._WALL, 1)
+
+            # Position the floor sprites
+            wall.center_x = self._COLUMN_SPACING + self._LEFT_MARGIN + 1000
+            wall.center_y = j * self._ROW_SPACING + self._BOTTOM_MARGIN 
+
+            # Add the floor to the lists
+            self.wall_list_v.append(wall)
+            self.wall_list_v.draw()
+
+        
+        arcade.draw_text('Instructions', constants.windowX -300, constants.windowY - 250, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
