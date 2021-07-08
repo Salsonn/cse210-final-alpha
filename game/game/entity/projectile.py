d8f986ef82189bd92514f3ecbf4384cb10452c43
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-07-08 16:42:27 -0600

    Added bouncing projectile mechanics and optimized velocity calculations.

--- game/game/entity/projectile.py
+++ game/game/entity/projectile.py
@@ -11,3 +11,17 @@ class Projectile(arcade.Sprite):
         self.center_y = startingY
         self.change_x = dX
         self.change_y = dY
+        self.bounces = 5 # add 1 for every bounce, -1 for infinite bouncing
+
+    def reflect(self, projectiles, l, r, t, b):
+        if True in {l, r}:
+            self.change_x = self.change_x * -0.5
+        if True in {t, b}:
+            self.change_y = self.change_y * -0.5
+        if not False in {l, r, t, b}:
+            self.center_x += self.change_x
+            self.center_y += self.change_y
+        if self.bounces == 0:
+            projectiles.remove(self)
+            print(f"Removed a projectile. There are now {len(projectiles)} on screen.")
+        else: self.bounces -= 1
--- game/game/handle_collisions_action.py
+++ game/game/handle_collisions_action.py
@@ -2,6 +2,7 @@ import random
 import arcade
 
 from game import constants
+from game.math import *
 from game.action import Action
 
 class HandleCollisionsAction(Action):
@@ -11,33 +12,18 @@ class HandleCollisionsAction(Action):
         Controller
     """
 
-    def execute(self, cast, reticle):
+    def execute(self, entities, reticle):
         """Executes the action using the given actors.
 
         Args:
-            cast (dict): The game actors {key: tag, value: list}.
+            entities (dict): The game actors {key: tag, value: list}.
         """
-        self._edgeDetection(cast["player"][0], cast["projectile"])
-        self._wallDetection(cast["player"][0], cast["projectile"], cast["wall"])
+        self._screenEdgeDetection(entities["player"][0], entities["projectile"])
+        if entities["player"][0].change_x or entities["player"][0].change_y: 
+            self._wallPlayerDetection(entities["player"][0], entities["projectile"], entities["wall"])
+        self._wallProjectileDetection(entities["projectile"], entities["wall"])
 
-        """paddle = cast["paddle"][0]
-
-        balls_to_remove = []
-
-        for ball in cast["balls"]:
-            self._handle_wall_bounce(ball)
-            self._handle_paddle_bounce(ball, paddle)
-
-            bricks = cast["bricks"]
-            self._handle_brick_collision(ball, bricks)
-
-            if constants.BALLS_CAN_DIE and self._is_off_screen(ball):
-                balls_to_remove.append(ball)
-
-        for ball in balls_to_remove:
-            cast["balls"].remove(ball)"""
-
-    def _edgeDetection(self, player, projectiles):
+    def _screenEdgeDetection(self, player, projectiles):
         # Prevent player from leaving sides of window
         if (player.center_x - (player._get_width() / 2)) <= 0 and player.change_x < 0:
             player.change_x = max(player.change_x, 0)
@@ -48,47 +34,57 @@ class HandleCollisionsAction(Action):
         if (player.center_y + (player._get_height() / 2)) >= constants.windowY and player.change_y > 0:
             player.change_y = min(player.change_y, 0)
 
-        for entity in projectiles:
-            if (entity.center_x + (entity._get_width() / 2) <= 0 or entity.center_x + (entity._get_width() / 2) >= constants.windowX) or (entity.center_y + (entity._get_height() / 2) <= 0 or entity.center_y + (entity._get_height() / 2) >= constants.windowY):
-                #projectiles[entity].remove_from_sprite_lists()
-                projectiles.remove(entity)
+        for projectile in projectiles:
+            if (projectile.center_x + (projectile._get_width() / 2) <= 0 or projectile.center_x + (projectile._get_width() / 2) >= constants.windowX) or (projectile.center_y + (projectile._get_height() / 2) <= 0 or projectile.center_y + (projectile._get_height() / 2) >= constants.windowY):
+                projectiles.remove(projectile)
                 print("Removed a projectile")
-
-    def _badwallDetection(self, player, projectiles, walls):
-        collisionList = []
-        for wall in walls:
-            #if player.center_x - (player._get_width() / 2) == wall.center_x + (wall._get_width() / 2) or player.center_x + (player._get_width() / 2) == wall.center_x - (wall._get_width() / 2) or player.center_y - (player._get_height() / 2) == wall.center_y + (wall._get_height() / 2) or player.center_y + (player._get_height() / 2) == wall.center_y - (wall._get_height() / 2):
-            if (player.center_x - (player._get_width() / 2) == wall.center_x + (wall._get_width() / 2) and player.center_y + (player._get_height() / 2) == wall.center_y - (wall._get_height() / 2)) or (player.center_y - (player._get_height() / 2) == wall.center_y + (wall._get_height() / 2) and player.center_x + (player._get_width() / 2) == wall.center_x - (wall._get_width() / 2)):
-                collisionList.append(wall)
-            if arcade.check_for_collision(player, wall):
-                collisionList.append(wall)
-        print(len(collisionList))
-        for entity in collisionList:
-            if entity.center_x < player.center_x:
-                player.change_x = max(player.change_x, 0)
-                print("collision detected from the left")
-            if entity.center_x > player.center_x:
-                player.change_x = min(player.change_x, 0)
-                print("collision detected from the right")
-            if entity.center_y < player.center_y:
-                player.change_y = max(player.change_y, 0)
-                print("collision detected from the top")
-            if entity.center_y > player.center_y:
-                player.change_y = min(player.change_y, 0)
-                print("collision detected from the bottom")
-        collisionList.clear()
     
-    def _wallDetection(self, player, projectiles, walls):
+    def _wallPlayerDetection(self, player, projectiles, walls):
         for entity in walls:
-            if ((self.bottomBound(entity) <= self.topBound(player) and self.topBound(player) <= self.topBound(entity)) or (self.bottomBound(entity) <= self.bottomBound(player) and self.bottomBound(player) <= self.topBound(entity))) and (self.leftBound(player, 1) <= self.rightBound(entity, 1) and self.rightBound(player, 1) >= self.leftBound(entity, 1)):
+            l, r, t, b = self._detectCollision(player, entity, 1)
+            if not False in {l, r, t, b}:
+                player.center_x += player.change_x * -1
+                player.center_y += player.change_y * -1
+            if l:
                 player.change_x = max(player.change_x, 0) # Stop leftward movement
-            if ((self.bottomBound(entity) <= self.topBound(player) and self.topBound(player) <= self.topBound(entity)) or (self.bottomBound(entity) <= self.bottomBound(player) and self.bottomBound(player) <= self.topBound(entity))) and (self.leftBound(entity, 1) <= self.rightBound(player, 1) and self.rightBound(entity, 1) >= self.leftBound(player, 1)):
-                player.change_x = min(player.change_x, 0) #, 1 Stop rightward movement
-            if ((self.leftBound(entity) <= self.rightBound(player) and self.rightBound(player) <= self.rightBound(entity)) or (self.leftBound(entity) <= self.leftBound(player) and self.leftBound(player) <= self.rightBound(entity))) and (self.bottomBound(player, 1) <= self.topBound(entity, 1) and self.topBound(player, 1) >= self.bottomBound(entity, 1)):
+                if constants.collisionDebug:
+                    print("Player cannot move left")
+            if r:
+                player.change_x = min(player.change_x, 0) # Stop rightward movement
+                if constants.collisionDebug:
+                    print("Player cannot move right")
+            if b:
                 player.change_y = max(player.change_y, 0) # Stop downward movement
-            if ((self.leftBound(entity) <= self.rightBound(player) and self.rightBound(player) <= self.rightBound(entity)) or (self.leftBound(entity) <= self.leftBound(player) and self.leftBound(player) <= self.rightBound(entity))) and (self.bottomBound(entity, 1) <= self.topBound(player, 1) and self.topBound(entity, 1) >= self.bottomBound(player, 1)):
+                if constants.collisionDebug:
+                    print("Player cannot move down")
+            if t:
                 player.change_y = min(player.change_y, 0) # Stop updward movement
+                if constants.collisionDebug:
+                    print("Player cannot move up")
+
+    def _wallProjectileDetection(self, projectiles, walls):
+        for wall in walls:
+            for projectile in projectiles:
+                # Skip precise collision math if objects are far apart. Didn't seem to help much though.
+                # if abs(projectile.center_x + projectile.change_x - wall.center_x + wall.change_x) >= 10 or abs(projectile.center_y + projectile.change_y - wall.center_y + wall.change_y) >= 10:
+                l, r, t, b = self._detectCollision(projectile, wall)
+                if True in {l, r, t, b}:
+                    projectile.reflect(projectiles, l, r, t, b)
+                # print(f"x:{projectile.change_x},y:{projectile.change_y}")
+                    
             
+    def _detectCollision(self, entity1, entity2, factorChange=0):
+        __left = __right = __top = __bottom = False
+        
+        if ((self.bottomBound(entity2) <= self.topBound(entity1) and self.topBound(entity1) <= self.topBound(entity2)) or (self.bottomBound(entity2) <= self.bottomBound(entity1) and self.bottomBound(entity1) <= self.topBound(entity2))) and ((self.leftBound(entity1, 1) <= self.rightBound(entity2, 1) and self.rightBound(entity1, factorChange) >= self.leftBound(entity2, factorChange))):
+            __left = True
+        if ((self.bottomBound(entity2) <= self.topBound(entity1) and self.topBound(entity1) <= self.topBound(entity2)) or (self.bottomBound(entity2) <= self.bottomBound(entity1) and self.bottomBound(entity1) <= self.topBound(entity2))) and ((self.leftBound(entity2, 1) <= self.rightBound(entity1, 1) and self.rightBound(entity2, factorChange) >= self.leftBound(entity1, factorChange))):
+            __right = True
+        if ((self.leftBound(entity2) <= self.rightBound(entity1) and self.rightBound(entity1) <= self.rightBound(entity2)) or (self.leftBound(entity2) <= self.leftBound(entity1) and self.leftBound(entity1) <= self.rightBound(entity2))) and ((self.bottomBound(entity1, 1) <= self.topBound(entity2, 1) and self.topBound(entity1, factorChange) >= self.bottomBound(entity2, factorChange))):
+            __bottom = True
+        if ((self.leftBound(entity2) <= self.rightBound(entity1) and self.rightBound(entity1) <= self.rightBound(entity2)) or (self.leftBound(entity2) <= self.leftBound(entity1) and self.leftBound(entity1) <= self.rightBound(entity2))) and ((self.bottomBound(entity2, 1) <= self.topBound(entity1, 1) and self.topBound(entity2, factorChange) >= self.bottomBound(entity1, factorChange))):
+            __top = True
+        return __left, __right, __top, __bottom
 
 
     def rightBound(self, entity, factorChange=0):
@@ -99,39 +95,3 @@ class HandleCollisionsAction(Action):
         return entity.center_y + (entity.change_y * factorChange) + (entity._get_height() / 2) 
     def bottomBound(self, entity, factorChange=0):
         return entity.center_y + (entity.change_y * factorChange) - (entity._get_height() / 2) 
-
-
-    def _handle_wall_bounce(self, ball):
-        ball_x = ball.center_x
-        ball_y = ball.center_y
-
-        # Check for bounce on walls
-        if ball_x <= 0 or ball_x >= constants.MAX_X:
-            ball.bounce_vertical()
-
-        if ball_y >= constants.MAX_Y:
-            ball.bounce_horizontal()
-        
-        if not constants.BALLS_CAN_DIE and ball_y <= 0:
-            ball.bounce_horizontal()
-    
-    def _handle_paddle_bounce(self, ball, paddle):
-        # This makes use of the `Sprite` functionality
-        if paddle.collides_with_sprite(ball):
-            # Ball and paddle collide!
-            ball.bounce_horizontal()
-
-    def _handle_brick_collision(self, ball, bricks):
-        brick_to_remove = None
-
-        for brick in bricks:
-            # This makes use of the `Sprite` functionality
-            if ball.collides_with_sprite(brick):
-                ball.bounce_horizontal()
-                brick_to_remove = brick
-        
-        if brick_to_remove != None:
-            bricks.remove(brick_to_remove)
-
-    def _is_off_screen(self, ball):
-        return ball.center_y < 0
