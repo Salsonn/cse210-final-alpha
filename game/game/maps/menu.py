a78392c84adf51fcff4e7009b047fe5054447cd5
Josh Thieme <jomthieme@gmail.com>
2021-07-10 19:42:15 -0600

    LOTS OF CHANGES

--- game/game/maps/menu.py
+++ game/game/maps/menu.py
@@ -6,22 +6,29 @@ class MainMenu():
 
     def __init__(self):
         self._BACKGROUND_COLOR = arcade.color.BLACK
-        self._FLOOR = './images/floor_tile_sprite.png'
-        self._FLOOR_W = 32
-        self._FLOOR_H = 32
-        self._WALL = './images/wall_tile_sprite.png'
+        self._FONT_COLOR = arcade.color.PALE_BLUE
+        
+        self._TITLE = 'WELCOME TO THE GAME'
+        self._INFO = 'CHOOSE WHAT TO DO'
+
+        self._FLOOR = './images/catacombs/cata_v1.0/mainlevbuild.png'
+        self._FLOOR_W = 65
+        self._FLOOR_H = 48
+        
+        self._WALL = './images/catacombs/cata_v1.0/mainlevbuild.png'
         self._WALL_W = 32
         self._WALL_H = 32
-        self._FONT_COLOR = arcade.color.PALE_BLUE
         self._WALL_COLOR = arcade.color.RED
         self._WALL_COLOR_2 = arcade.color.CYBER_YELLOW
         self._LEFT_WALL_X = 0
         self._LEFT_WALL_Y = constants.windowY / 2
-        self._TITLE = 'WELCOME TO THE GAME'
-        self._RADIUS = 150
+
+        self._POT = './images/TX_Props.png'
+        self._POT_W = 24
+        self._POT_H = 36
+        
         #SOUND = arcadeload_sound('/sounds/Tada-soundmp3')
-        self._INFO = 'CHOOSE WHAT TO DO'
-        self._TEMP = arcade.color.GREEN
+        
         self._COLUMN_SPACING = 20
         self._ROW_SPACING = 20
         self._LEFT_MARGIN = 110
@@ -29,24 +36,29 @@ class MainMenu():
         self._TILE_SPACING = 1.6
 
     def drawMap(self):
-        
-        # Draw the Walls
+    
+        MainMenu.draw_edges(self)
+        MainMenu.draw_walls(self)
+        MainMenu.draw_messages(self)
+        MainMenu.draw_floor(self)
+        MainMenu.draw_decor(self)
+
+    def draw_edges(self):
+        # Draw the edges
         arcade.draw_rectangle_filled(self._LEFT_WALL_X, self._LEFT_WALL_Y, 20, constants.windowY, self._WALL_COLOR)
         arcade.draw_rectangle_filled(constants.windowX, self._LEFT_WALL_Y, 20, constants.windowY, self._WALL_COLOR)
         arcade.draw_rectangle_filled(constants.windowX / 2, constants.windowY, constants.windowX, 20, self._WALL_COLOR)
         arcade.draw_rectangle_filled(constants.windowX / 2, 0, constants.windowX, 20, self._WALL_COLOR)
 
-        # Welcome Message
-        arcade.draw_text(self._INFO, constants.windowX / len(self._TITLE) + 100, constants.windowY - 40, self._FONT_COLOR, 25, 340, 'center', 'calibri', True)
-
+    def draw_walls(self):
+        
         # Draw Walls to choose what to do next
         self.wall_list_h = arcade.SpriteList()
-
         # Top left wall
-        for i in range(constants.windowY // self._WALL_H - 6):
+        for i in range(constants.windowY // self._WALL_H - 4):
 
             # Create the floor instance
-            wall = arcade.Sprite(self._WALL, 1)
+            wall = arcade.Sprite(self._WALL, 1,736.0,320.0,self._WALL_W, self._WALL_H)
 
             # Position the floor sprites
             wall.center_x = i * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 140
@@ -56,11 +68,12 @@ class MainMenu():
             self.wall_list_h.append(wall)
         self.wall_list_h.draw()
 
+
         # Top right wall
         for l in range(constants.windowY // self._WALL_H - 4):
 
             # Create the floor instance
-            wall = arcade.Sprite(self._WALL, 1)
+            wall = arcade.Sprite(self._WALL, 1,736.0,320.0,self._WALL_W, self._WALL_H)
 
             # Position the floor sprites
             wall.center_x = l * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) + 520
@@ -70,24 +83,98 @@ class MainMenu():
             self.wall_list_h.append(wall)
         self.wall_list_h.draw()
 
-        arcade.draw_text('Level 1', 430, constants.windowY - 90, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
 
-             # Draw Walls to choose what to do next
+        # Draw Walls to choose what to do next
         self.wall_list_v = arcade.SpriteList()
 
         # Right Vertical Wall
-        for j in range(constants.windowY // self._WALL_H - 8):
+        for j in range(constants.windowY // self._WALL_H - 4):
 
             # Create the floor instance
-            wall = arcade.Sprite(self._WALL, 1)
+            wall = arcade.Sprite(self._WALL, 1,736.0,320.0,self._WALL_W, self._WALL_H)
 
             # Position the floor sprites
-            wall.center_x = (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) + 1035
+            wall.center_x = (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) + 1032
             wall.center_y = j * (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) - 140
 
             # Add the floor to the lists
             self.wall_list_v.append(wall)
         self.wall_list_v.draw()
 
+        # Left Vertical Wall
+        for k in range(constants.windowY // self._WALL_H - 4):
+
+            # Create the floor instance
+            wall = arcade.Sprite(self._WALL, 1,736.0,320.0,self._WALL_W, self._WALL_H)
+
+            # Position the floor sprites
+            wall.center_x = (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 172
+            wall.center_y = k * (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) - 140
+
+            # Add the floor to the lists
+            self.wall_list_v.append(wall)
+        self.wall_list_v.draw()
+
+        # Bottom Wall
+        for m in range(constants.windowX // self._WALL_H - 3):
+
+            # Create the floor instance
+            wall = arcade.Sprite(self._WALL, 1,736.0,320.0,self._WALL_W, self._WALL_H)
+
+            # Position the floor sprites
+            wall.center_x = m * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 110
+            wall.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) - 172
+
+            # Add the floor to the lists
+            self.wall_list_h.append(wall)
+        self.wall_list_h.draw()
+
         
-        arcade.draw_text('Instructions', constants.windowX - 270, constants.windowY - 160, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
+
+    def draw_floor(self):
+        self.floor_list = arcade.SpriteList()
+        for i in range(constants.windowY // self._FLOOR_H + 1):
+            for j in range(constants.windowX // self._FLOOR_W + 16):
+
+                # Create the floor instance
+                floor = arcade.Sprite(self._WALL, 1,384.0,400.0,self._FLOOR_W, self._FLOOR_H)
+
+                # Position the floor sprites
+                floor.center_x = j * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 92
+                floor.center_y = i * (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) - 101
+
+                # Add the floor to the lists
+                self.floor_list.append(floor)
+        self.floor_list.draw()
+
+    def draw_messages(self):
+        
+        # Welcome Message
+        arcade.draw_text(self._INFO, constants.windowX / len(self._TITLE) + 100, constants.windowY - 40, self._FONT_COLOR, 25, 340, 'center', 'calibri', True)
+
+        # Level Message
+        arcade.draw_text('Level 1', 470, constants.windowY - 40, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
+        
+        # Instruction message
+        arcade.draw_text('Instructions', constants.windowX - 470, constants.windowY - 40, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
+
+    def draw_decor(self):
+
+        # Draw Arch for Level1
+        arch1 = arcade.Sprite(self._WALL, 1, 640.0, 0.0,80,96)
+        arch1.center_x = 27 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 65
+        arch1.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 430
+        arch1.draw()
+
+        # Draw Arch for Instructions
+        arch2 = arcade.Sprite(self._WALL, 1, 640.0, 0.0,80,96)
+        arch2.center_x = 16.5 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 65
+        arch2.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 430
+        arch2.draw()
+
+        # Draw Pottery
+        pot = arcade.Sprite(self._POT, 1, 164, 216, self._POT_W, self._POT_H)
+        pot.center_x = 2 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 65
+        pot.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 30
+        pot.draw()
+ 
