26f837c8a22127fc77b9f4f1ee360da91c368c87
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-07-17 22:28:10 -0600

    Someone(tm) messed up their comments. This fixes that problem permanently.

--- game/game/maps/instructions.py
+++ game/game/maps/instructions.py
@@ -1,4 +1,5 @@
 from game.maps.level1 import Level1
+from game.entity.trigger import Trigger
 import arcade
 from game import constants
 
@@ -48,17 +49,23 @@ class Instructions:
         arcade.draw_text(self._LINE4, constants.windowX / len(self._LINE4) + 300, constants.windowY - 240, self._FONT_COLOR, 25, 400, 'center', 'calibri', True)
 
     def draw_doors(self):
-        # Draw Arch for Main MENU
-        arch1 = arcade.Sprite(self._ARCH, 1, 398.0, 14.0,84,93)
-        arch1.center_x = 4 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 80
-        arch1.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 405
-        arch1.draw()
 
         # Draw Arch for Level 1
-        arch2 = arcade.Sprite(self._ARCH, 1, 398.0, 14.0,84,93)
-        arch2.center_x = 31 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 84
-        arch2.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 405
-        arch2.draw()
+        level1arch = arcade.Sprite(self._ARCH, 1, 398.0, 14.0,84,93)
+        level1arch.center_x = 4 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 80
+        level1arch.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 405
+        level1arch.draw()
+        menuArch = arcade.Sprite(self._ARCH, 1, 398.0, 14.0,84,93)
+        menuArch.center_x = 31 * (self._COLUMN_SPACING * self._TILE_SPACING) + (self._LEFT_MARGIN * self._TILE_SPACING) - 84
+        menuArch.center_y = (self._ROW_SPACING * self._TILE_SPACING) + (self._BOTTOM_MARGIN * self._TILE_SPACING) + 405
+        menuArch.draw()
+
+        # Draw Arch for Main MENU
+        level1Loader = Trigger(level1arch.center_x, level1arch.center_y + 20, 80, 76, self, 1)
+        self._triggers.append(level1Loader)
+
+        menuLoader = Trigger(menuArch.center_x, menuArch.center_y + 20, 80, 76, self, 2)
+        self._triggers.append(menuLoader)
 
     def draw_messages(self):
         # Draw Level 1  text for Arch
@@ -66,3 +73,9 @@ class Instructions:
         
         # Draw Main Menu  text for Arch
         arcade.draw_text(self._MAIN, constants.windowX - 370, constants.windowY - 60, self._FONT_COLOR, 20, 340, 'center', 'calibri', True)
+        
+    def handleTrigger(self, actionIndex):
+        if actionIndex == 1:
+            self._levelController.changeLevel(1)
+        elif actionIndex == 2:
+            self._levelController.changeLevel(0)
